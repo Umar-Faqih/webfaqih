04730dad27e321a0be0118118b8bb9eca11df6d5
Faqih <101651095+Umar-Faqih@users.noreply.github.com>
2025-08-03 00:33:10 +0700

    Update script.js

--- latihan/script.js
+++ latihan/script.js
@@ -1,10 +1,20 @@
+const audio = document.getElementById('audio');
 const playButton = document.getElementById('playButton');
 const playButton2 = document.getElementById('playButton2');
-const audio = document.getElementById('audio');
 
-playButton.addEventListener('click', function () {
-  audio.play();
-});
-playButton2.addEventListener('click', function () {
-  audio.pause();
-});
+// Toggle music
+function toggleMusic() {
+  if (audio.paused) {
+    audio.play();
+    playButton.style.backgroundImage = "url('img/pause.png')";
+    playButton2.style.backgroundImage = "url('img/pause.png')";
+  } else {
+    audio.pause();
+    playButton.style.backgroundImage = "url('img/play.png')";
+    playButton2.style.backgroundImage = "url('img/play.png')";
+  }
+}
+
+// Event listeners
+playButton.addEventListener('click', toggleMusic);
+playButton2.addEventListener('click', toggleMusic);

ba98a96623804144b682ae0b3c7ac7b466858577
Faqih <101651095+Umar-Faqih@users.noreply.github.com>
2025-08-03 00:59:24 +0700

    Update script.js

--- latihan/script.js
+++ latihan/script.js
@@ -6,15 +6,16 @@ const playButton2 = document.getElementById('playButton2');
 function toggleMusic() {
   if (audio.paused) {
     audio.play();
-    playButton.style.backgroundImage = "url('img/pause.png')";
-    playButton2.style.backgroundImage = "url('img/pause.png')";
+    playButton.style.backgroundImage = "url('img/true.png')";
+    playButton2.style.backgroundImage = "url('img/true.png')";
   } else {
     audio.pause();
-    playButton.style.backgroundImage = "url('img/play.png')";
-    playButton2.style.backgroundImage = "url('img/play.png')";
+    playButton.style.backgroundImage = "url('img/false.png')";
+    playButton2.style.backgroundImage = "url('img/false.png')";
   }
 }
 
 // Event listeners
 playButton.addEventListener('click', toggleMusic);
 playButton2.addEventListener('click', toggleMusic);
+
